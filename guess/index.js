dfa920d2bd2205f7d2fc3f8a755573cb8f3d1821
Newien Gopie <33331871+NSG-Company@users.noreply.github.com>
2021-04-12 12:55:33 +0200

    Delete index.js

--- guess/index.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    const person = prompt("Welkom! Wat is je naam?", "vul hier je naam in");
-
-    if (person != null) {
-        document.getElementById("demo").innerHTML =
-            "Hallo " + person + "! How are you today?";
-    }
-}
